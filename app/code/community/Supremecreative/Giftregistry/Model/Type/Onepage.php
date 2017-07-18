95b3e446067019690e5485a5bcc9cad331f3e68e
amir_canteetu <amircanteetu@gmail.com>
2017-07-18 09:55:25 +0200

    Commit 1.0.0.1 Extended Onepage model to save registrant owner's default address as shipping address oon checkout

---
+++ app/code/community/Supremecreative/Giftregistry/Model/Type/Onepage.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Supremecreative (Pty) Ltd
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to amir@supremecreative.co.za so I can send you a copy.
+ *
+ * @copyright   Copyright (c) 2017 Supremecreative (Pty) Ltd
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Supremecreative_Giftregistry_Model_Type_Onepage extends Mage_Checkout_Model_Type_Onepage {
+    
+     /**
+     * Save checkout shipping address
+     *
+     * @param   array $data
+     * @param   int $customerAddressId
+     * @return  Mage_Checkout_Model_Type_Onepage
+     */
+    public function saveShipping($data, $customerAddressId)
+    {
+        
+        $session    = Mage::getSingleton('giftregistry/session');
+        if(!$sharedGiftregistry = $session->getSharedGiftRegistry()) {
+            return parent::saveShipping($data, $customerAddressId);
+        }
+        
+        if (empty($data)) {
+            return array('error' => -1, 'message' => Mage::helper('checkout')->__('Invalid data.'));
+        }
+        $address = $this->getQuote()->getShippingAddress();
+
+        /* @var $addressForm Mage_Customer_Model_Form */
+        $addressForm    = Mage::getModel('customer/form');
+        $addressForm->setFormCode('customer_address_edit')
+            ->setEntityType('customer_address')
+            ->setIsAjaxRequest(Mage::app()->getRequest()->isAjax());
+
+        if (!empty($customerAddressId)) {
+            $customerAddress = Mage::getModel('customer/address')->load($customerAddressId);
+            if ($customerAddress->getId()) {
+
+                $address->importCustomerAddress($customerAddress)->setSaveInAddressBook(0);
+                $addressForm->setEntity($address);
+                $addressErrors  = $addressForm->validateData($address->getData());
+                if ($addressErrors !== true) {
+                    return array('error' => 1, 'message' => $addressErrors);
+                }
+            }
+        } else {
+            $addressForm->setEntity($address);
+            // emulate request object
+            $addressData    = $addressForm->extractData($addressForm->prepareRequest($data));
+            $addressErrors  = $addressForm->validateData($addressData);
+            if ($addressErrors !== true) {
+                return array('error' => 1, 'message' => $addressErrors);
+            }
+            $addressForm->compactData($addressData);
+            // unset shipping address attributes which were not shown in form
+            foreach ($addressForm->getAttributes() as $attribute) {
+                if (!isset($data[$attribute->getAttributeCode()])) {
+                    $address->setData($attribute->getAttributeCode(), NULL);
+                }
+            }
+
+            $address->setCustomerAddressId(null);
+            // Additional form data, not fetched by extractData (as it fetches only attributes)
+            $address->setSaveInAddressBook(empty($data['save_in_address_book']) ? 0 : 1);
+            $address->setSameAsBilling(empty($data['same_as_billing']) ? 0 : 1);
+        }
+
+        $address->implodeStreetAddress();
+        $address->setCollectShippingRates(true);
+
+        if (($validateRes = $address->validate())!==true) {
+            return array('error' => 1, 'message' => $validateRes);
+        }
+
+        $this->_setCartCouponCode();
+
+        $this->getQuote()->collectTotals()->save();
+
+        $this->getCheckout()
+            ->setStepData('shipping', 'complete', true)
+            ->setStepData('shipping_method', 'allow', true);
+
+        return array();
+    }   
+    
+}
